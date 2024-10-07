705248c4f81525c6ab5838dcaec3b41bf1363792
LuisEnrique9 <81138694+LuisEnrique9@users.noreply.github.com>
2024-10-06 22:18:11 -0600

    Update hTrabajo7.js

--- hTrabajo7.js
+++ hTrabajo7.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-import express from 'express';
 const jwt = requiere ('jsonwebtoken');
 requiere('dotenv').config();
 
